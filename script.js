67efd649365f4363cf8f2be72c99b1a80dfe82e1
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-11-06 17:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+let data = ["Item1", "Item2","Item3", "Item4","Item5", "Item6","Item7", "Item8","Item9", "Item10"]
+let liste = document.getElementById("infi-list");
 
+function renderList() {
+	liste.innerHTML = ""
+	data.map( (item)=> {
+		liste.innerHTML += `
+			<li>${item}</li>
+		`
+	})
+}
+
+
+liste.addEventListener("scrollend", (event) => {
+	let length = data.length;
+	
+	data.push(`Item${length+1}`)
+	data.push(`Item${length+2}`)
+	renderList();
+	
+});
+
+
+renderList();
